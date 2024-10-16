ca01483ad780a078cedc82c5aa012db4caae5e12
DevXCoder47 <nbelokrinitskiy@ukr.net>
2024-10-16 14:27:52 +0300

    Minimum requirements met

---
+++ ASP.NET_lesson_1_project/Controllers/MyProfileController.cs
@@ -0,0 +1,23 @@
+﻿using ASP.NET_lesson_1_project.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_lesson_1_project.Controllers
+{
+	public class MyProfileController : Controller
+	{
+		public MyProfile profile = new MyProfile
+		{
+			FirstName = "Nikita",
+			LastName = "Belokrinitskiy",
+			DateOfBirth = new DateTime(1998, 3, 10),
+			Age = 26,
+			Frontend = ["HTML", "CSS", "JavaScript"],
+			Backend = ["C++", "C#", "PHP"],
+			Frameworks = [".NET", "Entity Framework Core", "Laravel", "Unity"]
+		};
+		public IActionResult GetMyProfileView()
+		{
+			return View(profile);
+		}
+	}
+}
---
+++ ASP.NET_lesson_1_project/Models/MyProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace ASP.NET_lesson_1_project.Models
+{
+	public class MyProfile
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public DateTime DateOfBirth { get; set; }
+		public int Age { get; set; }
+		public string[] Frontend { get; set; }
+		public string[] Backend { get; set; }
+		public string[] Frameworks { get; set; }
+	}
+}
